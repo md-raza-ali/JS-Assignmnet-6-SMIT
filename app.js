3ac2f16fc4efcaad7af48448f22f84298210b82e
Md-RazaAli <muhammadrazaa263@gmail.com>
2023-10-11 08:32:29 -0700

    Js task

---
+++ app.js
@@ -0,0 +1,20 @@
+// CHAPTER 35 TO 38
+
+// ANSWER NO: 01
+// function currentDate() {
+//   let date = new Date();
+//   document.write(date);
+// }
+// currentDate();
+
+// ANSWER NO: 02
+// function greet(firstName, lastName) {
+//   let fullName = `${firstName} ${lastName}`;
+//   alert("Hello " + fullName);
+// }
+// let firstName = prompt("Enter your first name..");
+// let lastName = prompt("Enter your last name..");
+// greet(firstName, lastName);
+
+// ANSWER NO: 03
+
